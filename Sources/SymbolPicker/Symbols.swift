ce9e2677fafa43bd55d03abb86cf291dde2db5a2
Cody Lewis <cody@codylewis.dev>
2023-02-26 14:08:14 +1100

    feat: add structure for containing symbols with helper function to read resource text file

---
+++ Sources/SymbolPicker/Symbols.swift
@@ -0,0 +1,44 @@
+//
+//  Symbols.swift
+//  
+//
+//  Created by Cody Lewis on 2023-02-26.
+//
+
+import Foundation
+
+/// Named collection of SF Symbols which can use displayed in the `SymbolPicker` view.
+public struct SymbolGroup {
+    var name: String
+    var symbols: [String]
+    
+    /// Read a resource text file into an array of strings (split by new line in text file).
+    /// Expects each line to contain the `systemName` for SF Symbols.
+    private func fetchSymbols(_ filename: String) -> [String] {
+        guard let path = Bundle.module.path(forResource: filename, ofType: "txt"),
+              let content = try? String(contentsOfFile: path) else {
+            return []
+        }
+        return content
+            .split(separator: "\n")
+            .map({ String($0) })
+    }
+    
+    /// Initilise `SymbolGroup` using an array of symbol names (in string form).
+    /// Useful when trying to make a small custom set of symbols for a specific use case.
+    public init(_ name: String = "Symbols", symbols: [String]) {
+        self.name = name
+        self.symbols = symbols
+    }
+    
+    /// Initialise `SymbolGroup` using a `.txt` file in the `Resources` directory.
+    /// Useful when making a large custom set of symbols which are used extensively.
+    public init(_ name: String = "Symbols", filename: String) {
+        self.name = name
+        self.symbols = []
+        self.symbols = self.fetchSymbols(filename)
+    }
+}
+
+/// All symbols in the SF Symbols v4 catalogue.
+let DefaultSymbols = SymbolGroup(filename: "SFSymbols")
--- Tests/SymbolPickerTests/SymbolPickerTests.swift
+++ Tests/SymbolPickerTests/SymbolPickerTests.swift
@@ -6,6 +6,15 @@ final class SymbolPickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SymbolPicker().text, "Hello, World!")
+//        XCTAssertEqual(SymbolPicker().text, "Hello, World!")
+    }
+}
+
+final class SymbolsTests: XCTestCase {
+    func testDefault() throws {
+        // Ensure that the DefaultSymbols name attribute is default value
+        XCTAssertEqual(DefaultSymbols.name, "Symbols")
+        // Ensure that the DefaultSymbols object contains 4014 symbols in the `symbols` attribute
+        XCTAssertEqual(DefaultSymbols.symbols.count, 4014)
     }
 }
